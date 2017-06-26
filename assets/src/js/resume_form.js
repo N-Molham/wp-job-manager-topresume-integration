3db38efaa0be1a8bec890b4bd084523849b2422e
Nabeel Molham <n.molham@gmail.com>
2017-06-26 17:02:21 +0200

    Fix new resume first/last name binding

--- assets/src/js/resume_form.js
+++ assets/src/js/resume_form.js
@@ -3,6 +3,16 @@
  */
 (function ( w, $, undefined ) {
 	$( function () {
+		var $form       = $( '#submit-resume-form' ),
+		    $full_name  = $( '#candidate_name' ),
+		    $first_last = $( '#candidate_first_name, #candidate_last_name' );
+
+		$first_last.on( 'change keyup wpjm-tri-change', function () {
+			// $('#candidate_name')
+			$full_name.val( $first_last.filter( '#candidate_first_name' ).val() + ' ' + $first_last.filter( '#candidate_last_name' ).val() );
+			console.log( $full_name.val() );
+		} ).first().trigger( 'wpjm-tri-change' );
+
 		$( '.resume-manager-add-row' ).on( 'click wpjm-click', function ( e ) {
 			setTimeout( function () {
 				$( '#submit-resume-form' ).find( '.resume-manager-data-row .jmfe-date-picker:not(.hasDatepicker)' ).each( function ( index, input ) {
@@ -11,7 +21,7 @@
 			}, 100 );
 		} );
 
-		$( '#submit-resume-form' ).find( '.fieldset-candidate_education, .fieldset-candidate_experience' ).on( 'change', '.jmfe-date-picker', function ( e ) {
+		$form.find( '.fieldset-candidate_education, .fieldset-candidate_experience' ).on( 'change', '.jmfe-date-picker', function ( e ) {
 			var $entry_row = $( e.currentTarget ).closest( '.resume-manager-data-row' );
 			$entry_row.find( '.fieldset-date input' ).val(
 				$entry_row.find( '.fieldset-start_date input' ).val()
